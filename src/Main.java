f1d02b35fd127118c4e1d4ed5f8332b0f2cb2ca1
B23-User <venera.abulkhair@gmail.com>
2021-10-01 09:04:50 -0700

    Changed loop condition старое условие было жестко прописано, изменили на динамическре условие

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    int[] arr = {1,2,3};
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
     }
