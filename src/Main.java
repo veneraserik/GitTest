bbcee82d21f3e6401a2ce331135d5e3b2fd8b7cd
altuk <al.tukanov@gmail.com>
2021-10-01 20:15:49 +0400

    добавили ввод размера с консоли и заполнение случ числами

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	    int[] arr = {1,2,3};
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter size");
+        int size = scn.nextByte();
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int)(Math.random()*10);
+        }
+
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
